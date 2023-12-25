c1b84d2cde87733608503190ad779b8b2a0a45a1
pol_windows <paul.parisot@epitech.eu>
2023-12-25 20:34:29 +0100

    Add: ToRoman function

--- enchanting/enchanter.lua
+++ enchanting/enchanter.lua
@@ -3,7 +3,34 @@
 local Enchantments = require("Enchantments")
 
 local ORDER = { "Top", "Middle", "Bottom" }
-local FORMAT = { "I", "II", "III", "IV", "V" }
+
+local function ToRoman(num)
+    assert(type(num) == "number")
+    local ROMAN_LITTERALS = {
+        { value = 1000, symbol = "M" },
+        { value = 900, symbol = "CM" },
+        { value = 500, symbol = "D" },
+        { value = 400, symbol = "CD" },
+        { value = 100, symbol = "C" },
+        { value = 90, symbol = "XC" },
+        { value = 50, symbol = "L" },
+        { value = 40, symbol = "XL" },
+        { value = 10, symbol = "X" },
+        { value = 9, symbol = "IX" },
+        { value = 5, symbol = "V" },
+        { value = 4, symbol = "IV" },
+        { value = 1, symbol = "I" },
+    }
+
+    local result = "";
+    for _, numeral in next, ROMAN_LITTERALS do
+        while (num >= numeral.value) do
+            result = result .. numeral.symbol
+            num = num - numeral.value
+        end
+    end
+    return result;
+end
 
 function tableFind(t, value)
 	for i, v in ipairs(t) do
@@ -110,7 +137,7 @@ local function GetAllPossibleEnchantments(power)
         __tostring = function(self)
             local buf = "-- LIST --\n"
             for _, item in ipairs(self) do
-                buf = buf .. SnakeToPascal(item.Name) .. " " .. tostring(FORMAT[item.Level]) .. "\n"
+                buf = buf .. SnakeToPascal(item.Name) .. " " .. tostring(ToRoman(item.Level)) .. "\n"
             end
             return string.sub(buf, 1, #buf - 1)
         end
@@ -145,7 +172,7 @@ local function EnchantProfile(bookshelvesAround)
             selected = possibleEnchants[tablePick(probaEnchants)]
         until tableFind(alreadyUsed, selected.Name) == -1
         table.insert(alreadyUsed, selected.Name)
-        print(SnakeToPascal(selected.Name) .. " " .. tostring(FORMAT[selected.Level]))
+        print(SnakeToPascal(selected.Name) .. " " .. tostring(ToRoman(selected.Level)))
     end
 end
 
